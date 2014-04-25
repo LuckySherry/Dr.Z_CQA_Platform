7fc8dff3ce3b5a4845a8619f7c6a965cab9653ec
LuckySherry <lxy402290553@gmail.com>
2014-04-26 00:02:17 +0800

    GUI Complete

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var url = require("url");
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
@@ -20,8 +21,54 @@ router.get('/userlist', function(req, res) {
     });
 });
 /*GET Result Page*/
-router.get('/result',function(req,res) {
+/*router.get('/result',function(req,res) {
     res.redirect('/result.html');
     res.end();
+});*/
+
+/* POST to Add User Service */
+router.get('/result', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+    // Get our form values. These rely on the "name" attributes
+    var Title = req.query.question;
+
+    // Set our collection
+   // var collection1 = db.get('raw');
+    //var collection2 = dbu.get('User');
+    var Data = db.collection('Raw').find({"AnswerID":"153586"});
+    db.collection('User').find(/*{"UserID":"8589"}*/).limit(10).toArray(function (errU,resultU){
+        db.collection('Raw').find(/*{"AnswerID":"153586"}*/).limit(4).toArray(function(err, result) {
+            res.render('result',{
+                title:Title,QAPair:result,RelatedUser:resultU
+
+            });console.log(result);
+        });
+    });
+
+    /*var User = db.collection('User').find({});
+    db.collection('User').find({"UserID":"8589"}).toArray(function(err, result) {
+        res.render('result',{
+            RelatedUser:result
+        });
+
+    });
+    res.render('result',{
+        title:Title,data:Data,RelatedUser:User
+    });*/
+
+    /*collection1.findOne({},{},function(e,docs){
+       res.render('result',{
+           data:docs
+       });
+       console.log(docs.UserURL);
+    });
+    collection2.findOne({},{},function(e,docs){
+        res.render('result',{
+            RelatedUser:docs
+        });
+    });*/
+
 });
 module.exports = router;
