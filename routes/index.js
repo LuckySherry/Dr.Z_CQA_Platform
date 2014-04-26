85678138a71d16554396e5a6ac766b4795834877
LuckySherry <lxy402290553@gmail.com>
2014-04-26 15:25:17 +0800

    Function Complete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var url = require("url");
+var path = require("path");
+var exec = require('child_process').exec;
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
@@ -33,42 +36,58 @@ router.get('/result', function(req, res) {
     var db = req.db;
     // Get our form values. These rely on the "name" attributes
     var Title = req.query.question;
-
+    if(Title.length==0){
+        Title = 'Your Input Nothing';
+    }
+    var method = req.query.mid;
+    console.log(method);
     // Set our collection
    // var collection1 = db.get('raw');
     //var collection2 = dbu.get('User');
     var Data = db.collection('Raw').find({"AnswerID":"153586"});
-    db.collection('User').find(/*{"UserID":"8589"}*/).limit(10).toArray(function (errU,resultU){
-        db.collection('Raw').find(/*{"AnswerID":"153586"}*/).limit(4).toArray(function(err, result) {
-            res.render('result',{
-                title:Title,QAPair:result,RelatedUser:resultU
+    var child1, child2;
+    var hash1 = hashCode(Title);
+    var param = '"'+Title+'" '+'E:\\LearningResource\\UROP\\FYPWebsite_Express_Ver\\tmp\\'+hash1+'tmp.json';
+    var command1 = 'java -jar E:\\LearningResource\\UROP\\FYPWebsite_Express_Ver\\jar\\ExpertsFind.jar';
+    var command2 = 'java -jar E:\\LearningResource\\UROP\\FYPWebsite_Express_Ver\\jar\\DrZAnswerExt.jar';
 
-            });console.log(result);
+    console.log('h: '+ hash1);
+
+
+
+    child1 = exec(command2+' '+param,function(error,stdout,stderr){
+        console.log(command2+' '+param);
+        console.log('stderr: '+stderr);
+        console.log('stdout: ' + stdout);
+        var array = JSON.parse(stdout);
+        console.log("array: "+ array);
+        child2 = exec(command1+' '+stdout,function(e,o,er){
+            console.log('UID: ' + o);
+            var user = JSON.parse(o);
+            db.collection('User').find({"UserID":{$in:user}}).limit(5).toArray(function (errU,resultU){
+                db.collection('Raw').find({"AnswerID":{$in:array}}).limit(10).toArray(function(err, result) {
+                    res.render('result',{
+                        title:Title,QAPair:result,RelatedUser:resultU
+
+                    });//console.log(result);
+                });
+            });
         });
+
     });
 
-    /*var User = db.collection('User').find({});
-    db.collection('User').find({"UserID":"8589"}).toArray(function(err, result) {
-        res.render('result',{
-            RelatedUser:result
-        });
 
-    });
-    res.render('result',{
-        title:Title,data:Data,RelatedUser:User
-    });*/
-
-    /*collection1.findOne({},{},function(e,docs){
-       res.render('result',{
-           data:docs
-       });
-       console.log(docs.UserURL);
-    });
-    collection2.findOne({},{},function(e,docs){
-        res.render('result',{
-            RelatedUser:docs
-        });
-    });*/
 
 });
+
+hashCode = function(str){
+    var hash = 0;
+    if (str.length == 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+};
 module.exports = router;
